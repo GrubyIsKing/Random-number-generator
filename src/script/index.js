c9bbf51f0752cd818d215e4df9cce34c4ac8cda4
GrubyIsKing <95412743+GrubyIsKing@users.noreply.github.com>
2022-03-26 23:00:02 +0100

    Add files via upload

---
+++ src/script/index.js
@@ -0,0 +1,23 @@
+const second = document.getElementById("second");
+const generator = document.getElementById("generator");
+const result = document.getElementById("result");
+const first = document.getElementById("first");
+
+const generate =()=>{
+    const v = parseInt(first.value);
+    const y = parseInt(second.value);
+    const x = Math.floor(Math.random()*(y - v + 1) + v);
+    result.innerHTML = x;
+}
+
+generator.addEventListener("click", generate);
+
+
+  
+
+
+
+
+
+
+
